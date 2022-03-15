5a945537f4a95af6e16bfb84c91832e828ef47cb
notarutabaga <abriannalynn0027@gmail.com>
2022-03-15 15:08:31 -0400

    basic transaction

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,26 +1,41 @@
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXListView;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
+import java.lang.reflect.Array;
 import java.text.DecimalFormat;
-import java.util.Currency;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 
 public class Controller {
     public GridPane buttonGrid;
-
-    public JFXListView itemsList;
+    public JFXListView<String> itemsList = new JFXListView<>();
     public JFXButton totalButton;
+
+    public Pane payPane;
+    public Label totalLabel;
+    public JFXButton exactButton;
+    public JFXButton roundedButton;
+    public JFXButton round2Button;
+    public JFXButton round3Button;
+    public JFXButton customButton;
 
     double total;
 
     LinkedHashMap<String, Element> elementsMap = new LinkedHashMap<>();
+    double[] cashAmounts = {1.0, 5.0, 10.0, 20.0, 30.0, 40.0, 50.0, 100.0};
 
     public void initialize() {
         loadElements();
         loadButtons();
         total = 0;
         totalButton.setText(moneyFormat());
+        payPane.setVisible(false);
+        itemsList.getItems().clear();
     }
 
     public String moneyFormat() {
@@ -30,7 +45,26 @@ public class Controller {
         decimalFormat.setMinimumFractionDigits(2);
 
         String format = decimalFormat.format(total);
-        System.out.println(format);
+        return format;
+    }
+
+    public String payFormat() {
+        String pattern = "- $#,###,###,###.## -";
+        DecimalFormat decimalFormat = new DecimalFormat(pattern);
+        decimalFormat.setDecimalSeparatorAlwaysShown(true);
+        decimalFormat.setMinimumFractionDigits(2);
+
+        String format = decimalFormat.format(total);
+        return format;
+    }
+
+    public String exactFormat(double amount) {
+        String pattern = "$#,###,###,###.##";
+        DecimalFormat decimalFormat = new DecimalFormat(pattern);
+        decimalFormat.setDecimalSeparatorAlwaysShown(true);
+        decimalFormat.setMinimumFractionDigits(2);
+
+        String format = decimalFormat.format(amount);
         return format;
     }
 
@@ -90,12 +124,58 @@ public class Controller {
             String name = tempButton.getText();
             Element element = elementsMap.get(name);
             double price = element.getPrice();
-            System.out.println(name + "    " + price);
-            itemsList.getItems().add(name + "    " + price);
+            itemsList.getItems().add(name + "    " + exactFormat(price));
             total += price;
             totalButton.setText(moneyFormat());
         });
 
         return button;
    }
+
+    public void startPayment() {
+        if (total > 0) {
+            payPane.setVisible(true);
+            totalLabel.setText(payFormat());
+
+            exactButton.setText(exactFormat(total));
+
+            double rounded = Math.ceil(total);
+            int i;
+            if (rounded == total) {
+                i = nextGreatest(rounded);
+                rounded = cashAmounts[i];
+            }
+            roundedButton.setText(exactFormat(rounded));
+
+            int round2 = nextGreatest(rounded);
+            if (round2 == -1) {
+                round2Button.setText("");
+                round3Button.setText("");
+            } else {
+                round2Button.setText(exactFormat(cashAmounts[round2]));
+                int round3 = nextGreatest(cashAmounts[round2]);
+                if (round3 == -1) {
+                    round3Button.setText("");
+                } else {
+                    round3Button.setText(exactFormat(cashAmounts[round3]));
+                }
+            }
+        }
+    }
+
+    public int nextGreatest(double amount) {
+        int len = cashAmounts.length;
+
+        for (int i = 0; i < len; i++) {
+            if (cashAmounts[i] > amount) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public void finishPayment() {
+        initialize();
+    }
 }
